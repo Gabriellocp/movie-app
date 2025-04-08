7c861efa31dac93a1f886d49dc90e4136e4ab4df
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-08 14:18:10 -0300

    fix: Removed unnecessary movieId from Folder model

---
+++ api/prisma/migrations/20250408171825_remove_movieid_from_folder_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `movid_id` on the `folders` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "folders" DROP COLUMN "movid_id";
