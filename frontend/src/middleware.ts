7827ed1b5689efa1f5ee0c7befbf4de30c0e6bc7
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-10 17:45:43 -0300

    fix: Change middleware to deal with guest and private routes

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const privateRoutes = ['/movie']
+const guestRoutes = ['/']
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get('accessToken')?.value;
+    if (!token && privateRoutes.includes(request.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+    console.log(token, guestRoutes.includes(request.nextUrl.pathname))
+    if (token && guestRoutes.includes(request.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/movie', request.url))
+    }
+    return NextResponse.next()
+    // return NextResponse.redirect(new URL('/home', request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+}
