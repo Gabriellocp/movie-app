0b4f0336b9e2267eab849e76d90fb293a435755f
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-10 17:45:23 -0300

    refactor: Change Header styles

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -4,8 +4,8 @@ import { LoggedUserPopover } from "../LoggedUserPopover";
 export function Header() {
   const { activeUser } = useAuthContext();
   return (
-    <header className="flex p-5 mb-10 justify-between bg-gray-800 items-center">
-      <h1 className="text-white">Movie Planner</h1>
+    <header className="flex p-5 mb-10 justify-between bg-primary items-center sticky top-0">
+      <h1 className="text-white text-2xl font-medium">Movie Planner</h1>
       {!!activeUser && <LoggedUserPopover />}
     </header>
   );
