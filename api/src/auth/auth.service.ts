5b1107bc25b525817fe89053f62e802388205016
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-10 14:55:18 -0300

    feat: Add name on jwt return

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import { compare } from 'bcryptjs';
 import { UserService } from 'src/user/user.service';
 import { SignInDto } from './dto/signIn.dto';
@@ -11,14 +12,14 @@ export class AuthService {
     private readonly userService: UserService,
     private readonly jwtService: JwtService
   ) { }
-  private async generateJwt(id: string) {
-    const payload = { sub: id }
+  private async generateJwt(user: User) {
+    const payload = { sub: user.id, name: user.name }
     const accessToken = await this.jwtService.signAsync(payload)
     return accessToken
   }
   async signUp(dto: SignUpDto) {
     const user = await this.userService.create(dto)
-    return { accessToken: await this.generateJwt(user.id) }
+    return { accessToken: await this.generateJwt(user) }
   }
 
   async signIn(dto: SignInDto) {
@@ -31,7 +32,7 @@ export class AuthService {
     if (!validPass) {
       throw new UnauthorizedException('Invalid credentials')
     }
-    return { accessToken: await this.generateJwt(user.id) }
+    return { accessToken: await this.generateJwt(user) }
   }
 
 
