e62b7a1599c15656ac3e4db404ac0a2462419429
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-14 17:21:17 -0300

    feat: Create update function on UserService

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -1,9 +1,14 @@
+import { MovieDb, UserMovie } from "@/types"
 import { api } from "./utils/api"
 
 class UserService {
     private readonly handler = api('/user')
-    async addMovie(movie: any) {
+    async addMovie(movie: MovieDb) {
         await this.handler.post('/add', movie)
+    }
+    async updateMovie(id: string, movie: UserMovie) {
+        const response = await this.handler.put(`/update/${id}`, movie)
+        return response.data
     }
     async getMovies() {
         const result = await this.handler.get('/movies')
