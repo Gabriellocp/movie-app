888d1c03d10330f196adf68bc79d5ef5debe7d18
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-14 17:20:01 -0300

    feat: Add Movie types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,3 +3,25 @@ export enum MOVIE_STATUS {
     WATCH_LATER = 'WATCH_LATER',
 
 }
+
+export type MovieDb = {
+    id: number;
+    original_title: string;
+    title: string;
+    overview: string;
+    genres: { name: string, id: number }[],
+    poster_path: string
+}
+
+export type MovieApi = {
+    id: string;
+    externalId: string;
+    folderId: string;
+    status: MOVIE_STATUS;
+    annotation: string;
+    rate: number;
+    createdAt: Date;
+
+}
+
+export type UserMovie = MovieApi & Pick<MovieDb, Exclude<keyof MovieDb, 'id'>>
